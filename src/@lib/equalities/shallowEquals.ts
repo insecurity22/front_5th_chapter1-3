7625e6eeb19aca0c7b1d23e80a8b0e6b2a8a28d9
insecurity22 <insecurity@naver.com>
2025-04-10 03:42:58 +0900

    chore: fix typo in comments

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,36 +1,35 @@
-const isObject = (obj: unknown): obj is Record<string, unknown> => {
-  return obj !== null && typeof obj === "object";
-};
-
+/**
+ * shallowEquals 함수는 두 값의 얕은 (===) 비교를 수행합니다.
+ */
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  // 1. 배열을 얕게 비교
+  // 배열 얕은 비교
   if (Array.isArray(objA) && Array.isArray(objB)) {
-    // 길이 확인
+    //
     if (objA.length !== objB.length) return false;
 
-    // 각 요소가 동일한지 얕게 비교
+    // 배열의 각 요소 얕은 비교
     return objA.every((value, index) => value === objB[index]);
   }
 
-  // 2. 객체를 얕게 비교
+  // 객체 얕은 비교
   if (isObject(objA) && isObject(objB)) {
     const entriesA = Object.entries(objA);
     const entriesB = Object.entries(objB);
-    /**
-        Object.entries({ a: 1, b: 2 }) = [['a', 1], ['b', 2]]
-     */
 
+    // 키 개수가 다르면 두 객체가 같을 수 없음
     if (entriesA.length !== entriesB.length) return false;
-    /**
-        서로 다른 객체이지만 동일한 값을 가지는 경우 true
-        { a: 1, b: 2 } === { a: 1, b: 2 }
-     */
 
     return entriesA.every(([key, value]) => {
+      // 다른 객체지만 동일한 값을 가지는 경우
+      // { a: 1, b: 2 } === { a: 1, b: 2 }
       return objB[key] === value;
     });
   }
 
-  // 3. 참조 비교
+  // 참조 비교
   return objA === objB;
 }
+
+const isObject = (obj: unknown): obj is Record<string, unknown> => {
+  return obj !== null && typeof obj === "object";
+};
