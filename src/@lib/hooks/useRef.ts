b5efe5036ae8d4a0a630483f799b91e7c2d1da59
insecurity22 <insecurity@naver.com>
2025-04-11 03:41:07 +0900

    feat: useRef 훅 추가

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
+/**
+ * useRef 훅
+ * 렌더링 사이에 값을 유지할 수 있는 변경 가능한 레퍼런스(참조) 생성
+ * current 속성을 통해 값에 접근하고 변경할 수 있게 함.
+ */
 export function useRef<T>(initialValue: T): { current: T } {
-  // React의 useState를 이용해서 만들어보세요.
   const [value] = useState({ current: initialValue });
   return value;
 }
